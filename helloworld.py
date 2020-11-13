ad5e1747036e1b84563764fcaa784bfa35d3e331
laoyu-wq <1989477874@qq.com>
2020-11-14 00:55:57 +0800

    c

---
+++ helloworld.py
@@ -0,0 +1,60 @@
+# python practice
+print("Hello, World!")
+
+if True:
+   print("Answer") 
+   print("True")
+else:
+   print("Answer")    
+   print("False")
+
+# raw_input("按下 enter 键退出，其他任意键显示...\n")
+
+a = b = c = 1
+d=4j
+print (a)
+print (b)
+print (c)
+s="dfcgvhjk"
+str1=['sbwh',str(a+b),'woshiteyiying']##列表(可以改里面的元素):有序对象的集合
+str2=('sbwh',str(a+b),'woshiteyiying')##元组（不可以更改里面的文字）
+
+str1[0]='fwehcnjs' 
+# str2[1]=46378674
+
+print(str1[0])
+print(str)
+print(str1[2:])
+print(str1[:2])
+print(str1*2)
+
+print(complex(a+d))
+
+
+dict = {}
+
+dict['one'] = "This is one"
+dict[2] = "This is two"
+dict[2] = "\erfd"
+dict[3] = "This is daciedesan"
+ 
+tinydict = {'name': 'runoob','code':6734, 'dept': 'sales'}
+
+
+print (dict['one'] )         # 输出键为'one' 的值
+print(dict)
+print( dict[2]       )       # 输出键为 2 的值
+print (tinydict)             # 输出完整的字典
+print (tinydict.keys())      # 输出所有键
+print (tinydict.values())    # 输出所有值
+print('\r\n')
+print(set(str1))
+print("\r\n")
+str2=str1[:]
+print(str1)
+print(str2)
+if str1 ==  str2 :##用于判断两个字符是否相等
+ print ('febnjfb')
+if str1 is  str2 :##身份描述符（勇于判断两个变量是否来自同一个（同一块内存空间））
+ print (hex(48))
+
